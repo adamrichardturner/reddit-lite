a63a0eed6cd00837dc8d45a96ed9ea0f715ac2dc
Adam Turner <hello@adamrichardturner.dev>
2022-07-28 19:25:33 +0100

    Initial store setup

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import postsSlice from '../components/posts/postsSlice';
 import postSlice from '../components/posts/post/postSlice';
+import topicsSlice from '../components/topics/topicsSlice';
 
 export const store = configureStore({
   reducer: {
     posts: postsSlice,
     post: postSlice,
+    topics: topicsSlice
   },
 });
