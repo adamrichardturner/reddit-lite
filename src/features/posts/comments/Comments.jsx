99972e5464c6b3c1067a6cc6957768b8237dd58b
Ellie <112726692+elliezub@users.noreply.github.com>
2023-08-08 15:47:13 -0400

    Update Comments.jsx

--- src/features/posts/comments/Comments.jsx
+++ src/features/posts/comments/Comments.jsx
@@ -1,32 +1,35 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import Styles from './Comments.css';
-import { getPostComments } from '../../../util/Reddit';
-import { timePassed } from '../post/Post';
+import React from "react";
+import { useState, useEffect } from "react";
+import Styles from "./Comments.css";
+import { getPostComments } from "../../../util/Reddit";
+import { timePassed } from "../post/Post";
 
-export const Comments = ({permalink}) => {
-    const [comments, setComments] = useState([]);
-    // getPostComments and add to state
-    useEffect(() => {
-        getPostComments(permalink).then(jsonComments => setComments(...comments, jsonComments));
-    }, [permalink]);
+export const Comments = ({ permalink }) => {
+  const [comments, setComments] = useState([]);
+  // getPostComments and add to state
+  useEffect(() => {
+    getPostComments(permalink).then((jsonComments) =>
+      setComments(...comments, jsonComments)
+    );
+  }, [comments, permalink]);
 
-    return (
-        <div className="commentsContainer" style={Styles}>
-            <h2>Comments</h2>
-            { // Iterate over comments and render comment elements
-                Object.values(comments).map(comment => (
-                    <>
-                        <div className="commentsBody">
-                            <p>{comment.body}</p>
-                        </div>
-                        <div className="commentsFooter">
-                            <p>Author: {comment.author}</p>
-                            <p>{timePassed(comment.created_utc)}</p>
-                        </div>
-                    </>
-                ))
-            }
-        </div>
-    )
+  return (
+    <div className="commentsContainer" style={Styles}>
+      <h2>Comments</h2>
+      {
+        // Iterate over comments and render comment elements
+        Object.values(comments).map((comment) => (
+          <>
+            <div className="commentsBody">
+              <p>{comment.body}</p>
+            </div>
+            <div className="commentsFooter">
+              <p>Author: {comment.author}</p>
+              <p>{timePassed(comment.created_utc)}</p>
+            </div>
+          </>
+        ))
+      }
+    </div>
+  );
 };
